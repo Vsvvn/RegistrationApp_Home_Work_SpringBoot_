18324e82e26d56c0e38e3d0c955862177b7db345
Vsvvn <Vladsun@yandex.ru>
2024-02-06 04:06:00 +0300

    Initial commit

---
+++ src/main/java/app/registration/RegistrationAppHomeWorkSpringBootApplication.java
@@ -0,0 +1,13 @@
+package app.registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationAppHomeWorkSpringBoot1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistrationAppHomeWorkSpringBoot1Application.class, args);
+    }
+
+}
---
+++ src/test/java/app/registration/RegistrationAppHomeWorkSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package app.registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationAppHomeWorkSpringBoot1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
